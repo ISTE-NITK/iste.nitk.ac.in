e30f7f17be0d480a503bc7391c17d342c1a46c3e
ArjunAnilPillai <arjunanilpillai@gmail.com>
2021-01-07 02:13:47 +0530

    NerfPiano updated

--- nerfpiano.py
+++ nerfpiano.py
@@ -2,13 +2,19 @@ import importlib,os
 import django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'website.settings'
 django.setup()
-from App.models import Game
+from App.models import Game, User
 from django.core.files.images import ImageFile
 
 objGame = Game.objects.filter(gameId = '3')
 for i in objGame:
-	i.score = i.score//2
+	initial = i.score
+	userObj = i.name
+	i.score = (i.score+1)//2
 	if i.score > 250:
 		i.score = 250
 	i.save()
+	#print(i.name.name, i.score, initial)
+	userObj.score -= initial - i.score
+	userObj.save()
+
 	
